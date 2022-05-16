c2f7368506e92f1aeca256d5370b47af74fcf133
Ukaoha Chizoba <ukaohachizoba6@gmail.com>
2022-05-16 13:34:04 +0100

    added search functionality

---
+++ script.js
@@ -0,0 +1,90 @@
+"use strict";
+// let userName = "";
+const url = `https://api.github.com/users/ukaoha`;
+
+const usernameContainer = document.querySelector(".username_container");
+// const InputValue = document.querySelector(".search__data").value;
+
+const searchUser = document.querySelector(".search_btn");
+
+
+
+ searchUser.addEventListener('click' , function() {
+  let InputValue = document.querySelector(".search-data").value;
+  fetch(`https://api.github.com/users/${InputValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      renderUsers(data);
+    });
+
+})
+
+
+
+
+const renderUsers = function (usersData) {
+  console.log(usersData);
+
+  usernameContainer.innerHTML = ` <div class="bio-update">
+  <div>
+    <img class="profile_img" src="${usersData.avatar_url}" alt="" />
+  </div>
+  <div class="bio">
+    <div>
+    <h2>NAME: ${usersData.name}</h2>
+    <p>${usersData.login}</p>
+    <!-- <p>This profile has no bio</p> -->
+    </div>
+    <div>
+      <p class="date_joined">${usersData.created_at}</p>
+    </div>
+  </div>
+
+  </div>
+  <div class="bio_wrap">
+    <p>${usersData.bio}</p>
+  </div>
+  <div class="repos__container">
+    <div class="repos">
+      <h4>Public Repos</h4>
+      <h2>${usersData.public_repos}</h2>
+    </div>
+    <div class="repos">
+      <h4>Followers</h4>
+      <h2>${usersData.followers}</h2>
+    </div>
+    <div class="repos">
+      <h4>Following</h4>
+      <h2>${usersData.following}</h2>
+    </div>
+  </div>
+
+  <div class="location__container">
+    <div class="location">
+      <span><i class="fa-solid fa-location-dot"></i>${usersData.location}</span>
+    </div>
+    <div class="location">
+    <span><i class="fa-brands fa-twitter-square"></i><a target="_blank" href="${usersData.twitter_username}">Twitter</a></span>
+  </div>
+  <div class="location">
+  <span> <i class="fa-solid fa-link"></i><a target="_blank" href="${usersData.html_url}">Visit profile</a></span>
+  </div>
+  <div class="location">
+      <span><i class="fa-solid fa-building"></i>${usersData.company}</span>
+    </div>
+  </div> `
+};
+
+// fetchUsers();
+
+
+
+
+
+
+
+
+
+
+
